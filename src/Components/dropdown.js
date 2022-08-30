5a93d5ce3bd9f0c6135f573c44e26cd9df3b26ae
Cindy Tan <ctan@dminc.com>
2022-08-30 12:15:43 -0500

    src files

---
+++ src/Components/dropdown.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const defaultProps = {
+    id: "",
+    optionsList: [],
+    disabled: false,
+    defaultValue: "",
+};
+function default_1(props = defaultProps) {
+    const getOption = (value) => {
+        const option = document.createElement("option");
+        option.setAttribute("value", value);
+        option.setAttribute("label", value);
+        if (value === props.defaultValue) {
+            option.setAttribute("selected", value);
+        }
+        return option;
+    };
+    const getDropdown = () => {
+        const input = document.createElement("select");
+        input.setAttribute("type", "text");
+        input.setAttribute("id", props.id);
+        input.setAttribute("disabled", props.disabled ? "disabled" : null);
+        input.setAttribute("selected", props.id);
+        props.optionsList.forEach((value) => {
+            input.appendChild(getOption(value));
+        });
+        return input;
+    };
+    getDropdown();
+}
+exports.default = default_1;
+//# sourceMappingURL=dropdown.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import dropdown from "./Components/dropdown";
+(() => {
+  dropdown;
+})();
